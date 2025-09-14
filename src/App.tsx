bc66cca2278a26bbb4ec85f2c95f4bc86571ab39
kyle-tightest <kyletightest@gmail.com>
2025-09-14 17:16:43 +0200

    update brent pic

--- src/App.tsx
+++ src/App.tsx
@@ -882,7 +882,7 @@ was something special.
                 },
                 {
                   name: "Brent Lechet",
-                  img: "/img/brent4.jpg"
+                  img: "/img/brent5.jpg"
                 },
                 {
                   name: "Luke Kramer",
